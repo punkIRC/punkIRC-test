a827c0db91f6d319307b64f25c1bce8a2f37ee2d
Schrotty <rubenmaurer@live.de>
2018-09-12 17:46:13 +0200

    Finish 'main' javaDoc

--- src/main/java/de/rubenmaurer/punk/Pricefield.java
+++ src/main/java/de/rubenmaurer/punk/Pricefield.java
@@ -4,10 +4,25 @@ import de.rubenmaurer.punk.util.CLI;
 import de.rubenmaurer.punk.util.Terminal;
 import org.fusesource.jansi.AnsiConsole;
 
+/**
+ * The main entry point.
+ *
+ * @author Ruben Maurer
+ * @version 1.0
+ * @since 1.0
+ */
 public class Pricefield {
 
+    /**
+     * The runtime ID.
+     */
     public static String ID;
 
+    /**
+     * Main entry point.
+     *
+     * @param args start arguments
+     */
     public static void main(String[] args) {
         AnsiConsole.systemInstall();
 
--- src/main/java/de/rubenmaurer/punk/Settings.java
+++ src/main/java/de/rubenmaurer/punk/Settings.java
@@ -6,20 +6,48 @@ import de.rubenmaurer.punk.util.Terminal;
 import java.io.*;
 import java.util.*;
 
+/**
+ * Class for accessing various settings.
+ *
+ * @author Ruben Maurer
+ * @version 1.0
+ * @since 1.0
+ */
 public class Settings {
 
+    /**
+     * Properties set by user.
+     */
     private Properties properties = new Properties();
 
+    /**
+     * Properties set by dev.
+     */
     private Properties internal = new Properties();
 
+    /**
+     * Properties for version.
+     */
     private Properties version = new Properties();
 
+    /**
+     * Settings singleton.
+     */
     private static Settings self = new Settings();
 
+    /**
+     * Collection of settings overrides.
+     */
     private static Map<String, String> overrides = new HashMap<>();
 
+    /**
+     * List of tests to execute.
+     */
     private static List<Class> tests = new LinkedList<>();
 
+    /**
+     * Create new settings and init. everything needed.
+     */
     private Settings() {
         String props = "resources/config.properties";
         String inter = "resources/system.properties";
@@ -48,20 +76,38 @@ public class Settings {
         }
     }
 
+    /**
+     * Add a test to the list of tests.
+     *
+     * @param tests the test to add
+     */
     public static void storeTests(List<Class> tests) {
         Settings.tests.addAll(tests);
     }
 
+    /**
+     * Generate a runtiome id.
+     *
+     * @return the id
+     */
     public static String generateID() {
         Pricefield.ID = String.valueOf(System.nanoTime()).substring(0, 5);
 
         return Pricefield.ID;
     }
 
+    /**
+     * Get the current path.
+     *
+     * @return the path
+     */
     private static String path() {
         return new File(ClassLoader.getSystemClassLoader().getResource(".").getPath()).getAbsolutePath();
     }
 
+    /**
+     * Create all needed directories and pipe the System.err stream.
+     */
     static void checkDirectoriesAndPipe() {
         try {
             File logDir = new File(String.format("%s", Settings.logs()));
@@ -90,34 +136,67 @@ public class Settings {
         }
     }
 
+    /**
+     * Load an override.
+     *
+     * @param key the override key
+     * @return the override value
+     */
     private static String loadOverride(String key) {
         return overrides.getOrDefault(key, "none");
     }
 
+    /**
+     * Store an override.
+     *
+     * @param key the override key
+     * @param value the override value
+     */
     public static void storeOverride(String key, String value) {
         overrides.put(key, value);
     }
 
+    /**
+     * Get the version.
+     *
+     * @return the version
+     */
     public static String version() {
         return self.version.getProperty("version", "1.0");
     }
 
+    /**
+     * Get the build number.
+     *
+     * @return the build number
+     */
     public static String build() {
         return self.version.getProperty("build", "501");
     }
 
+    /**
+     * Get the message delimiter.
+     *
+     * @return the delimiter.
+     */
     public static String delimiter() {
         return self.internal.getProperty("delimiter");
     }
 
+    /**
+     * Get the logs path.
+     *
+     * @return the logs path
+     */
     public static String logs() {
         return String.format("%s/%s", path(), self.internal.getProperty("logs"));
     }
 
-    public static String results() {
-        return String.format("%s/%s", path(), self.internal.getProperty("results"));
-    }
-
+    /**
+     * Get the executable path.
+     *
+     * @return the paath
+     */
     public static String executable() {
         String ovr = loadOverride("executable");
         if (!ovr.equals("none")) {
@@ -127,6 +206,11 @@ public class Settings {
         return String.format("%s/%s", path(), self.properties.getProperty("executable"));
     }
 
+    /**
+     * Get the hostname.
+     *
+     * @return the hostname
+     */
     public static String hostname() {
         String ovr = loadOverride("hostname");
         if (!ovr.equals("none")) {
@@ -136,26 +220,53 @@ public class Settings {
         return self.properties.getProperty("hostname");
     }
 
+    /**
+     * Load a start parameter.
+     *
+     * @param id parameter id
+     * @return the parameter
+     */
     private static String parameter(int id) {
         return self.properties.getProperty(String.format("parameter_%d", id));
     }
 
+    /**
+     * Create a server setup for a non-java server.
+     *
+     * @param server the server
+     * @return the setup
+     */
     public static String[] serverSetup(String server) {
         return new String[] {
                 server, parameter(0), parameter(1), parameter(2)
         };
     }
-
+    /**
+     * Create a server setup for a java server.
+     *
+     * @param server the server
+     * @return the setup
+     */
     public static String[] javaServerSetup(String server) {
         return new String[] {
           "java", "-jar", server, parameter(0), parameter(1), parameter(2)
         };
     }
 
+    /**
+     * Get the list of tests to execute.
+     *
+     * @return the tests
+     */
     public static List<Class> tests() {
         return tests;
     }
 
+    /**
+     * Use java mode?
+     *
+     * @return the use java mode?
+     */
     public static boolean java() {
         String ovr = loadOverride("java");
         if (!ovr.equals("none")) {
@@ -165,6 +276,11 @@ public class Settings {
         return Boolean.valueOf(self.properties.getProperty("java"));
     }
 
+    /**
+     * Get the port.
+     *
+     * @return the port
+     */
     public static int port() {
         String ovr = loadOverride("port");
         if (!ovr.equals("none")) {
@@ -174,34 +290,72 @@ public class Settings {
         return Integer.parseInt(self.properties.getProperty("port"));
     }
 
+    /**
+     * Get the max amount of shutdown tries.
+     *
+     * @return the amount of shutdown tries
+     */
     public static int shutdownTries() {
         return Integer.parseInt(self.internal.getProperty("shutdownTries"));
     }
 
+    /**
+     * Get the amount of expected lines.
+     *
+     * @return the expected lines
+     */
     public static int expectedLines() {
         return Integer.parseInt(self.internal.getProperty("expectedLines"));
     }
 
+    /**
+     * Get the timeout.
+     *
+     * @return the timeout
+     */
     public static int timeout() {
         return Integer.parseInt(self.properties.getProperty("timeout"));
     }
 
+    /**
+     * Get the start delay.
+     *
+     * @return the start delay
+     */
     public static int startDelay() {
         return Integer.parseInt(self.properties.getProperty("startDelay"));
     }
 
+    /**
+     * Get the debug value.
+     *
+     * @return is debug mode?
+     */
     public static boolean debug() {
         return Boolean.valueOf(loadOverride("log"));
     }
 
+    /**
+     * Get the stop delay.
+     *
+     * @return the stop delay
+     */
     public static int stopDelay() {
         return Integer.parseInt(self.properties.getProperty("stopDelay"));
     }
 
+    /**
+     * Get the amount of expected auth lines.
+     *
+     * @return the expected line count
+     */
     public static int authLines() {
         return Integer.parseInt(self.properties.getProperty("authLines"));
     }
 
+    /**
+     * Sleep.
+     */
     public static void sleep() {
         try {
             Thread.sleep(1000);
